c8f4ab3939c43797e50cf89d94e97e9ed0bddb8f
ayrokid <yanun@ayrokid>
2014-02-14 20:48:19 -0800

    yanun selesai

---
+++ book.js
@@ -0,0 +1,15 @@
+function buku($scope){
+    
+    $scope.book={
+        "java":{
+            "kode":"java",
+            "judul":"Java EE Programming",
+            "penulis":"Rahman dkk"
+        },
+        "php":{
+            "kode":"php",
+            "judul":"PHP Legacy code",
+            "penulis":"Ruzman"
+        }
+    };
+}
---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script type="text/javascript" src="angular.min.js"></script>
+    </head>
+    <body ng-app="">
+        <input type="text" placeholder="nama" ng-model="nama" />
+        <h3>{{"Hallo "+nama}}</h3>
+    </body>
+</html>
